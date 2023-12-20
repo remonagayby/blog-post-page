27be0914e0b5e2d86e62a761f673ab64971fbe9e
Remon Agayby <agayby.remon.com>
2023-12-20 13:44:46 +0200

    adding hide & save post btns

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function handleCreatPost() {
     document.getElementById("post-form-container").style.display = "block"
 }
+
+function hidePostForm() {
+    document.getElementById("post-form-container").style.display = "none"
+}
